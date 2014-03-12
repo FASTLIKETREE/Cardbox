27a6c05ee3e6fc83255bacd2c09006a8af487a41
FASTLIKETREE <wesleyaaronhunt@gmail.com>
2014-03-12 01:36:31 -0500

    persistent state for main game board support is done, need to add hand persistent state base on the IPaddress of the connecting client

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ function DeckOrCardObject()
 	this.x = null
 	this.y = null
 	this.room = null
+	this.deck = null
 	this.owner = null
 	this.facedown = null
 	this.cardarray = new Array()
@@ -98,6 +99,16 @@ function start(route, handle)
 		GameWindowConnectionObject[address.address] = socket
 		socket.join("mainwindows")
 		console.log("Main Window has been created");
+		for(var key in MasterCardAndDeckStateObject)
+		{
+			if(MasterCardAndDeckStateObject[key].deck == 0){
+				socket.emit("CreateObject", {"ObjectSrc":MasterCardAndDeckStateObject[key].cardarray[0], "ObjectID":key, "x":MasterCardAndDeckStateObject[key].x, "y":MasterCardAndDeckStateObject[key].y, "type":"card"})
+			}
+
+			if(MasterCardAndDeckStateObject[key].deck == 1){
+				socket.emit("CreateObject", {"ObjectID":key, "ObjectSrc":"null", "x":MasterCardAndDeckStateObject[key].x, "y":MasterCardAndDeckStateObject[key].y, "type":"deck"})
+			}
+		}
 	})
 
 	socket.on("HandWindowCreated", function(){
@@ -156,6 +167,7 @@ function start(route, handle)
 
 					MasterCardAndDeckStateObject[ObjectID] = new DeckOrCardObject()
 					MasterCardAndDeckStateObject[ObjectID].cardarray = cards
+					MasterCardAndDeckStateObject[ObjectID].deck = 1
 					MasterCardAndDeckStateObject[ObjectID].x = "0px"
 					MasterCardAndDeckStateObject[ObjectID].y = "200px"
 					console.log(MasterCardAndDeckStateObject)
@@ -168,6 +180,7 @@ function start(route, handle)
 				console.log(CDObject + "<-- this is the CDObject")
 				if(CDObject.cardarray.length > 0){
 					MasterCardAndDeckStateObject[ObjectID] = new DeckOrCardObject()
+					MasterCardAndDeckStateObject[ObjectID].deck = 0
 					MasterCardAndDeckStateObject[ObjectID].x = (Number((CDObject.x).replace("px", "")) + 360) + "px"
 					MasterCardAndDeckStateObject[ObjectID].y = Number((CDObject.y).replace("px", "")) + "px"
 					//sio.sockets.emit("CreateObject", {"ObjectSrc":CDObject.cardarray[0], "ObjectID":ObjectID, "x":MasterCardAndDeckStateObject[ObjectID].x, "y":MasterCardAndDeckStateObject[ObjectID].y, "type":"card"})
@@ -179,6 +192,7 @@ function start(route, handle)
 			case "drawhand":
 				if(CDObject.cardarray.length > 0){
 					MasterCardAndDeckStateObject[ObjectID] = new DeckOrCardObject()
+					MasterCardAndDeckStateObject[ObjectID].deck = 0
 					MasterCardAndDeckStateObject[ObjectID].x = "0px"
 					MasterCardAndDeckStateObject[ObjectID].y = "0px"
 					HandWindowConnectionObject[address.address].emit("CreateObject", {"ObjectSrc":MasterCardAndDeckStateObject[data.ObjectID].cardarray[0], "ObjectID":ObjectID, "x":MasterCardAndDeckStateObject[ObjectID].x, "y":MasterCardAndDeckStateObject[ObjectID].y, "type":"card"})
@@ -199,77 +213,6 @@ function start(route, handle)
 				break
 		}
     })
-
-	/*
-	socket.on("ToBoard", function(data){
-		console.log("To BOARD CALLED!!" + data.CardID)
-		console.log("To BOARD CALLED!!" + data.CardSrc)
-		console.log(MasterCardAndDeckStateObject)
-		MasterCardAndDeckStateObject[data.CardID].owner = null
-		sio.sockets.emit("ToBoard", {"CardSrc":data.CardSrc, "CardID":data.CardID})
-	})
-
-	socket.on("GrabCard", function(data){
-		console.log("Grab card to hand called!!!" + data.CardID)
-		console.log("Grab card to hand called!!!" + data.CardSRC)
-		//sio.sockets.emit("GrabCard", {"CardSRC":data.CardSRC, "CardID":data.CardID})
-		sio.sockets.emit("DeleteObject", {"CardSRC":data.CardSRC, "ObjectID":data.CardID})
-		MasterCardAndDeckStateObject[data.CardID].owner = address.address
-		HandWindowConnectionObject[address.address].emit("CardToHand", {"CardSrc":data.CardSRC, "CardID":data.CardID})
-	})
-*/
-
-	/*
-	socket.on("DrawCard", function(){
-		console.log(MasterCardAndDeckStateObject)
-		//MasterCardAndDeckStateObject[UniqueIDCounter] = new DeckOrCardObject()
-		var CDObject = MasterCardAndDeckStateObject[GameWindowConnectionObject[address.address].BoundDeckObject]
-
-		if(CDObject.cardarray.length > 0)
-		{
-			var ObjectID = null
-			if(IDReaperArray.length > 0){
-				console.log(IDReaperArray + "<-- thisi s the id reaper array")
-				ObjectID = IDReaperArray.splice(0,1)}
-			else{
-				ObjectID = UniqueIDCounter
-				UniqueIDCounter = UniqueIDCounter + 1
-			}
-
-			MasterCardAndDeckStateObject[ObjectID] = new DeckOrCardObject()
-			MasterCardAndDeckStateObject[ObjectID].x = (Number((CDObject.x).replace("px", "")) + 360) + "px"
-			MasterCardAndDeckStateObject[ObjectID].y = Number((CDObject.y).replace("px", "")) + "px"
-			sio.sockets.emit("DrawCard", {"CardSrc":CDObject.cardarray[0], "CardID":ObjectID, "x":MasterCardAndDeckStateObject[ObjectID].x, "y":MasterCardAndDeckStateObject[ObjectID].y})
-			MasterCardAndDeckStateObject[ObjectID].cardarray = CDObject.cardarray.splice(0,1)
-			console.log(MasterCardAndDeckStateObject)
-
-		}
-	})
-
-	socket.on("DrawCardHand", function(data){
-		var CDObject = MasterCardAndDeckStateObject[GameWindowConnectionObject[address.address].BoundDeckObject]
-		if(CDObject.cardarray.length > 0)
-		{
-			var ObjectID = null
-			if(IDReaperArray.length > 0){
-				console.log(IDReaperArray + "<-- thisi s the id reaper array")
-				ObjectID = IDReaperArray.splice(0,1)}
-			else{
-				ObjectID = UniqueIDCounter
-				UniqueIDCounter = UniqueIDCounter + 1
-			}
-
-			MasterCardAndDeckStateObject[ObjectID] = new DeckOrCardObject()
-			MasterCardAndDeckStateObject[ObjectID].x = "0px"
-			MasterCardAndDeckStateObject[ObjectID].y = "0px"
-			HandWindowConnectionObject[address.address].emit("CardToHand", {"CardSrc":MasterCardAndDeckStateObject[data.DeckID].cardarray[0], "CardID":ObjectID, "x":MasterCardAndDeckStateObject[ObjectID].x, "y":MasterCardAndDeckStateObject[ObjectID].y})
-			MasterCardAndDeckStateObject[ObjectID].cardarray = CDObject.cardarray.splice(0,1)
-			MasterCardAndDeckStateObject[ObjectID].owner = address.address
-		}
-	})
-
-*/
-
 
 	socket.on("PositionDeck", function(data){
 		if(typeof(MasterCardAndDeckStateObject[data.DeckID]) === "object" && MasterCardAndDeckStateObject[data.DeckID] != null)
@@ -366,6 +309,7 @@ function start(route, handle)
 						//PopulateDecksString = PopulateDecksString.slice(0, PopulateDecksString.length - 1)
 						//console.log(PopulateDecksString + "<-- this is the populate decks string")
 						socket.emit('DecksDir', PopulateDecksString);
+
 					}
 
 
