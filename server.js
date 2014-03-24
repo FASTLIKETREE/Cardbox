981ee4ac1628228a8fb9b251d8df244c44e87207
FASTLIKETREE <wesleyaaronhunt@gmail.com>
2014-03-24 18:51:26 -0500

    Dir works now

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ function start(route, handle)
 	var server = http.createServer(onRequest);
 	sio = io.listen(server)
 	sio.set("log level", 1)
-	server.listen(80)
+	server.listen(8080, "127.0.0.1")
 
 
 	sio.sockets.on('connection', function(socket){
